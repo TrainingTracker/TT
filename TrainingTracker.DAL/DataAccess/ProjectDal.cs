b03a9a68089c78f47494f37b4f285da38523ae5f
harshwPro <harshw1@gmail.com>
2016-12-03 17:04:28 +0530

    Session functions refactored from sp to ef.

--- TrainingTracker.DAL/DataAccess/ProjectDal.cs
+++ TrainingTracker.DAL/DataAccess/ProjectDal.cs
@@ -11,35 +11,6 @@ namespace TrainingTracker.DAL.DataAccess
 {
     public class ProjectDal:IProjectDal
     {
-        public List<Project> GetProjectsByUserId(int userId)
-        {
-            var projects = new List<Project>();
-
-            var prms = new List<SqlParameter>
-            {
-                SqlUtility.CreateParameter(SPGetProjectsByUserID.PARAM_USER_ID, SqlDbType.Int, userId)
-            };
-
-            try
-            {
-                var dt = SqlUtility.ExecuteAndGetTable(SPGetProjectsByUserID.NAME,
-                    CommandType.StoredProcedure, SPGetProjectsByUserID.TABLE_NAME, prms);
-
-                projects.AddRange(from DataRow row in dt.Rows
-                                  select new Project
-                                  {
-                                      Title = row["Title"].ToString(),
-                                      ProjectId = Convert.ToInt32(row["ProjectId"]),
-                                      UserProjectId = Convert.ToInt32(row["UserProjectId"])
-                                  });
-            }
-            catch (Exception ex)
-            {
-                LogUtility.ErrorRoutine(ex);
-            }
-
-            return projects;
-        }
-
+       
     }
 }
