39f6efd53ac2f5a6e478d9c037d3833fe01b83f2
harshwPro <harshw1@gmail.com>
2017-01-16 13:58:53 +0530

    Pagination implementation
    PagedResult class created to store paged results.
    Page extension method created to process paged data.
    Example implemented on GetCourses.

---
+++ TrainingTracker.DAL/DataAccess/CourseDal.cs
@@ -0,0 +1,15 @@
+﻿using TrainingTracker.DAL.EntityFramework;
+
+namespace TrainingTracker.DAL.DataAccess
+{
+    public class CourseDal
+    {
+        public PagedResult<Course> GetCourses(int pageNumber, int pageSize)
+        {
+            using (var context = new TrainingTrackerEntities())
+            {
+                return context.Courses.Page(pageNumber, pageSize);
+            }
+        }
+    }
+}
