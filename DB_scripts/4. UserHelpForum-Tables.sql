14cf1997ab6b07556c86c1f4af5ecd6950959c51
harshwPro <harshw1@gmail.com>
2017-02-03 16:55:09 +0530

    Script added for tables and lookup data

---
+++ DB_scripts/4. UserHelpForum-Tables.sql
@@ -0,0 +1,44 @@
+CREATE TABLE `ForumUserHelpCategories` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Title` varchar(250) CHARACTER SET utf8mb4 NOT NULL,
+  `Description` longtext CHARACTER SET utf8mb4 NOT NULL,
+  `CreatedOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ForumUserHelpStatus` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Title` varchar(250) CHARACTER SET utf8mb4 NOT NULL,
+  `CreatedOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ForumUserHelpThreads` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `PostId` int(11) NOT NULL,
+  `Description` longtext CHARACTER SET utf8mb4 NOT NULL,
+  `AddedBy` int(11) NOT NULL,
+  `CreatedOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`),
+  KEY `FK_ForumUserHelpThreads_User` (`AddedBy`),
+  KEY `FK_ForumUserHelpThreads_Post` (`PostId`),
+  CONSTRAINT `FK_ForumUserHelpThreads_Post` FOREIGN KEY (`PostId`) REFERENCES `ForumUserHelpPosts` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_ForumUserHelpThreads_User` FOREIGN KEY (`AddedBy`) REFERENCES `User` (`UserId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ForumUserHelpPosts` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Title` varchar(250) CHARACTER SET utf8mb4 NOT NULL,
+  `Description` longtext CHARACTER SET utf8mb4 NOT NULL,
+  `AddedBy` int(11) NOT NULL,
+  `CategoryId` int(11) NOT NULL,
+  `StatusId` int(11) NOT NULL,
+  `CreatedOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`),
+  KEY `FK_ForumUserHelpPosts_User` (`AddedBy`),
+  KEY `FK_ForumUserHelpPosts_Category` (`CategoryId`),
+  KEY `FK_ForumUserHelpPosts_Status` (`StatusId`),
+  CONSTRAINT `FK_ForumUserHelpPosts_Category` FOREIGN KEY (`CategoryId`) REFERENCES `ForumUserHelpCategories` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_ForumUserHelpPosts_Status` FOREIGN KEY (`StatusId`) REFERENCES `ForumUserHelpStatus` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_ForumUserHelpPosts_User` FOREIGN KEY (`AddedBy`) REFERENCES `User` (`UserId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8;
