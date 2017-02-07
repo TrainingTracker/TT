b9e4088c48e3f6df831ee2c83166e97595a9f598
udaychy <uday.choudhary.hsetin@gmail.com>
2017-02-07 16:47:45 +0530

    Modification in EmailAlertSubscription table
    Modification in EmailAlertSubscription table

--- DB_scripts/2.SubscribedTrainee/[Create]TraineeRelatedEmailAlertPreferences.sql
+++ DB_scripts/2.SubscribedTrainee/[Create]TraineeRelatedEmailAlertPreferences.sql
@@ -1,19 +1,19 @@
 
 DROP Table SubscribedTrainee;
 
-CREATE TABLE `TraineeReletedEmailAlertPreferences` (
+CREATE TABLE `EmailAlertSubscription` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `SubscribedByUserId` int(11) NOT NULL,
   `SubscribedForUserId` int(11) NOT NULL,
-  `IsSubscribedForComment` bit NOT NULL,
+  `IsSubscribedForComment` bit(1) NOT NULL,
   `IsSubscribedForWeeklyFeedback` bit(1) NOT NULL,
   `IsSubscribedForCodeReview` bit(1) NOT NULL,
   `IsSubscibedForAssignment` bit(1) NOT NULL,
   `IsSubscibedForSkill` bit(1) NOT NULL,
   `IsDeleted` bit(1) NOT NULL,
   PRIMARY KEY (`Id`),
-  KEY `FK_TraineeReletedEmailAlertPreferences_User_SubscribedBy` (`SubscribedByUserId`),
-  KEY `FK_TraineeReletedEmailAlertPreferences_User_SubscribedFor` (`SubscribedForUserId`),
-  CONSTRAINT `FK_TraineeReletedEmailAlertPreferences_User_SubscribedBy` FOREIGN KEY (`SubscribedByUserId`) REFERENCES `User` (`UserId`),
-  CONSTRAINT `FK_TraineeReletedEmailAlertPreferences_User_SubscribedFor` FOREIGN KEY (`SubscribedForUserId`) REFERENCES `User` (`UserId`)
-);
+  KEY `FK_EmailAlertSubscription_User_SubscribedBy` (`SubscribedByUserId`),
+  KEY `FK_EmailAlertSubscription_User_SubscribedFor` (`SubscribedForUserId`),
+  CONSTRAINT `FK_EmailAlertSubscription_User_SubscribedBy` FOREIGN KEY (`SubscribedByUserId`) REFERENCES `User` (`UserId`),
+  CONSTRAINT `FK_EmailAlertSubscription_User_SubscribedFor` FOREIGN KEY (`SubscribedForUserId`) REFERENCES `User` (`UserId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
