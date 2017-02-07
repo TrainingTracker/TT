d9ed405bce0ac1eac7a80b63bb687441224956ea
harshwPro <harshw1@gmail.com>
2017-02-07 11:16:29 +0530

    Script to add trainee discussion forum tables and data.

---
+++ DB_scripts/7. DiscussionForum-LookUpData.sql
@@ -0,0 +1,29 @@
+INSERT INTO `ForumDiscussionCategories`
+(`Title`,
+`Description`,
+`CreatedOn`)
+VALUES
+('TraineeQuestion',
+'',
+NOW());
+
+INSERT INTO `ForumDiscussionStatus`
+(`Title`,
+`CreatedOn`)
+VALUES
+('New',
+NOW());
+
+INSERT INTO `ForumDiscussionStatus`
+(`Title`,
+`CreatedOn`)
+VALUES
+('In Discussion',
+NOW());
+
+INSERT INTO `ForumDiscussionStatus`
+(`Title`,
+`CreatedOn`)
+VALUES
+('Closed',
+NOW());
