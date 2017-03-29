a946d49ae6c79c54dfe628f91eea9b4228887646
Shekhar Pankaj <shekharpankaj@outlook.com>
2017-03-29 19:18:32 +0530

    New servicesfor Mirror
    New servicesfor Mirror

--- TrainingTracker/Scripts/Custom/Services/MirrorServices.js
+++ TrainingTracker/Scripts/Custom/Services/MirrorServices.js
@@ -5,11 +5,16 @@
             return my.ajaxService.ajaxGetDeffered(my.rootUrl + "/Mirror/GetAssignmentFeedbackWithFilters?userId=" + userId + "&startDate=" + startDate + "&endDate=" + endDate, null);
         },
         getCodeReviewWithFiltersPromise: function(userId, startDate, endDate) {
+            return my.ajaxService.ajaxGetDeffered(my.rootUrl + "/Mirror/GetCodeReviewFeedbackWithFilters?userId=" + userId + "&startDate=" + startDate + "&endDate=" + endDate, null);
         },
         getRandomReviewWithFiltersPromise: function(userId, startDate, endDate) {
+            return my.ajaxService.ajaxGetDeffered(my.rootUrl + "/Mirror/GetSkillsFeedbackWithFilters?userId=" + userId + "&startDate=" + startDate + "&endDate=" + endDate, null);
         },
         getSkillsWithFiltersPromise: function(userId, startDate, endDate) {
+            return my.ajaxService.ajaxGetDeffered(my.rootUrl + "/Mirror/GetRandomReviewFeedbackWithFilters?userId=" + userId + "&startDate=" + startDate + "&endDate=" + endDate, null);
         },
         getWeeklyFeedbacksWithFiltersPromise: function(userId, startDate, endDate) {
+            return my.ajaxService.ajaxGetDeffered(my.rootUrl + "/Mirror/GetWeeklyFeedbackWithFilters?userId=" + userId + "&startDate=" + startDate + "&endDate=" + endDate, null);
         },
     };
+}(my));
